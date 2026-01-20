1048cb424e830a8e2f82c5e7389a7bbe124a57e4
Deviprasad <deviprasad@gmail.com>
2026-01-20 14:37:20 +0530

    it was unsved so saved it

--- src/Enterprise.OrderManagement.Infrastructure/Persistence/OrderRepository.cs
+++ src/Enterprise.OrderManagement.Infrastructure/Persistence/OrderRepository.cs
@@ -13,9 +13,7 @@ public sealed class OrderRepository : IOrderRepository
 	}
 	public async Task AddAsync( Order order, CancellationToken cancellationToken = default)
 	{
-
 		await _dbContext.Orders.AddAsync(order, cancellationToken);
 		await _dbContext.SaveChangesAsync(cancellationToken);
 	}
-
 }
